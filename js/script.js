27a0b85b3e934537f66d89398f784fd4e130f787
Chebanro2807 <Chebanro2807@ukr.net>
2021-05-05 13:13:09 +0300

    slider done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
+// for Serach
 $(document).on('click', '.search', function(){
     $('.search-bar').addClass('search-bar--active')
 });
 
 $(document).on('click', '.search-cancel', function(){
     $('.search-bar').removeClass('search-bar--active')
+});
+
+//login and singup form
+
+$(document).on('click', '.user,.already-account', function(){
+    $('.form').addClass('form__login--active').removeClass('form__sign-up--active')
+});
+
+$(document).on('click', '.sign-up-btn', function(){
+    $('.form').addClass('form__sign-up--active').removeClass('form__login--active')
+});
+
+$(document).on('click', '.form__cancel', function(){
+    $('.form').removeClass('form__login--active').removeClass('form__sign-up--active')
+});
+
+//Slider
+
+$(document).ready(function() {
+    $('#adaptive').lightSlider({
+        adaptiveHeight:true,
+        auto:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+    });
 });
